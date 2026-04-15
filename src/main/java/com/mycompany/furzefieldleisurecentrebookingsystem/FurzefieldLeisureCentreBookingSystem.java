6331083e782f0fcc62a9c81c6a6a81b1417daace
sivajeevan <sivajeevankiruparaj@gmail.com>
2026-04-15 11:08:52 +0100

    Initial project setup

---
+++ src/main/java/com/mycompany/furzefieldleisurecentrebookingsystem/FurzefieldLeisureCentreBookingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.furzefieldleisurecentrebookingsystem;
+
+/**
+ *
+ * @author Admin
+ */
+public class FurzefieldLeisureCentreBookingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
